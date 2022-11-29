a07c3f477b98ab4d843c82faed7156b10bfc34e6
c0a21155 <c0a21155fe@edu.teu.ac.jp>
2022-11-29 15:02:00 +0900

    練習4

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,21 +1,22 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
+#練習3
 def button_click(event):
     btn = event.widget
     num = btn["text"]
-    tkm.showinfo("", f"[{num}]ボタンが押されました")
+    tkm.showinfo("", f"{num}ボタンがクリックされました")
 
 #練習1
 root = tk.Tk()
 root.geometry("300x500")
 
-#entry = tk.Entry(root, )
-#entry.insert(tk.END, "fugapiyo")
+#練習４
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
+entry.grid(row=0, column=0, columnspan=3)
 
 #練習2
-r, c = 0, 0
+r, c = 1, 0
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
