7dbd55b1bed4edc152a6574c5c682374443efa1f
c0a21155 <c0a21155fe@edu.teu.ac.jp>
2022-11-29 18:40:26 +0900

    コメント#3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,9 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
-
-
 # 練習３
 def button_click(event):
     btn = event.widget
@@ -18,25 +15,26 @@ def button_click(event):
         # 練習６
         entry.insert(tk.END, num)
 
-def click_delete(event2):            #"="を右クリックすると、
-    click_right = event2.widget      #表示文字列を削除する。
+def click_delete1(event1):            #ボタン"del"を左クリックすると、
+    click_left = event1.widget      #表示文字列の右側1文字を削除する。
+    num = click_left["text"]
+    if num == "del":
+        delete = entry.get()
+        entry.delete(len(delete)-1,tk.END)
+
+def click_delete2(event2):            #ボタン"del"を右クリックすると、
+    click_right = event2.widget      #表示文字列を全て削除する。
     num = click_right["text"]
-    if num == "=":        
+    if num == "del":        
         entry.delete(0,tk.END)
-
-    
-
 
 # 練習１
 root = tk.Tk()
-root.geometry("300x500")
-
-
+root.geometry("300x600")
 
 # 練習４
 entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row=0, column=0, columnspan=3)
-
 
 # 練習２
 r, c = 1, 2                  #ボタンの順番を並び変える。
@@ -46,7 +44,6 @@ for num in range(9,-1,-1):
     button.bind("<1>", button_click)
     c -= 1
     if c == -1:
-          
         if r < 3:
             r += 1  
             c = 2
@@ -55,25 +52,17 @@ for num in range(9,-1,-1):
             c = 0
             r = 4
 
-
-
-
-
 # 練習５
-operators = ["+", "="]
+operators = ["+", "=", "del"]
 for ope in operators:
     c += 1
-    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("",30))
-    button.grid(row=r, column=c)
-    button.bind("<1>", button_click)
-    button.bind("<3>", click_delete) #click_deleteのイベント紐づけ
     if c%3 == 0:
         r += 1
         c = 0
-
-
-
-
-
-
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("",30))
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    button.bind("<1>", click_delete1) #click_delete1のイベント紐づけ
+    button.bind("<3>", click_delete2) #click_delete2のイベント紐づけ
+            
 root.mainloop()
