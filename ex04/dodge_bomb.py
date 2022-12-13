a07b08db8315b99e6b75f00c6b115c3b26182258
c0a21155 <c0a21155fe@edu.teu.ac.jp>
2022-12-13 17:34:52 +0900

    本番

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,11 +2,8 @@ import pygame as pg
 import random
 import sys
 
-
 def check_bound(obj_rct, scr_rct):
-    # 第1引数：こうかとんrectまたは爆弾rect
-    # 第2引数：スクリーンrect
-    # 範囲内：+1／範囲外：-1
+
     yoko, tate = +1, +1
     if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
         yoko = -1
@@ -14,44 +11,45 @@ def check_bound(obj_rct, scr_rct):
         tate = -1
     return yoko, tate
 
-
 def main():
     clock =pg.time.Clock()
-    # 練習１
+
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
     scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
-    # 練習３
-    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.image.load("fig/3.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
-    # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
+
     scrn_sfc.blit(tori_sfc, tori_rct) 
 
-    # 練習５
-    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc = pg.Surface((100, 100)) 
     bomb_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (50, 50), 50)
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx = random.randint(0, scrn_rct.width)
     bomb_rct.centery = random.randint(0, scrn_rct.height)
     scrn_sfc.blit(bomb_sfc, bomb_rct) 
     vx, vy = +1, +1
 
-    # 練習２
+    font = pg.font.SysFont("arial", 80)
+    text = font.render("DEATH", True, (255, 0, 0))
+
+
     while True:
+        vx *= 1.001
+        vy *= 1.001
         scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
-        # 練習4
-        key_dct = pg.key.get_pressed() # 辞書型
+        key_dct = pg.key.get_pressed()
         if key_dct[pg.K_UP]:
             tori_rct.centery -= 1
         if key_dct[pg.K_DOWN]:
@@ -60,8 +58,18 @@ def main():
             tori_rct.centerx -= 1
         if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
+        if key_dct[pg.K_w]:
+            if key_dct[pg.K_l]:
+                bomb_rct.centerx -= 2
+            if key_dct[pg.K_r]:
+                bomb_rct.centerx += 2
+        if key_dct[pg.K_g]:
+            if key_dct[pg.K_u]:
+                bomb_rct.centery -= 2
+            if key_dct[pg.K_d]:
+                bomb_rct.centery += 2
         if check_bound(tori_rct, scrn_rct) != (+1, +1):
-            # どこかしらはみ出ていたら
+
             if key_dct[pg.K_UP]:
                 tori_rct.centery += 1
             if key_dct[pg.K_DOWN]:
@@ -69,15 +77,29 @@ def main():
             if key_dct[pg.K_LEFT]:
                 tori_rct.centerx += 1
             if key_dct[pg.K_RIGHT]:
-                tori_rct.centerx -= 1            
+                tori_rct.centerx -= 1
+
+        #if check_bound(bomb_rct, scrn_rct) != (+2, +2):
+        #   if key_dct[pg.K_w]:
+        #        if key_dct[pg.K_l]:
+        #            bomb_rct.centerx += 2
+        #        if key_dct[pg.K_r]:
+        #            bomb_rct.centerx -= 2
+        #    if key_dct[pg.K_g]:
+        #        if key_dct[pg.K_u]:
+        #            bomb_rct.centery += 2
+        #        if key_dct[pg.K_d]:
+        #            bomb_rct.centery -= 2
+
         scrn_sfc.blit(tori_sfc, tori_rct) 
 
-        # 練習６
         bomb_rct.move_ip(vx, vy)
         scrn_sfc.blit(bomb_sfc, bomb_rct) 
         yoko, tate = check_bound(bomb_rct, scrn_rct)
         vx *= yoko
         vy *= tate
+
+        scrn_sfc.blit(text, (700, 450))
 
         if tori_rct.colliderect(bomb_rct):   #tori_rctとbomb_rctを逆にしてもOK
             return
